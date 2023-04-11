d34197db4ee08b6f2869b5a4370d4a1968717de4
tjk1898 <tjk1898@gmail.com>
2023-04-11 11:27:15 +0800

    project init.

---
+++ src/globalConfig.js
@@ -0,0 +1,4 @@
+export default {
+    apiUrl: "http://localhost:8080/",
+    appName: "Reggie Manager Frontend",
+};
--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+import "./styles/common.css";
+import "./styles/page.css";
+import "./styles/icon/iconfont.css";
+import store from './store';
+import globalConfig from "./globalConfig";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(ElementUI);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    router,
+    store,
+    provide: {
+        globalConfig,
+    },
+    render: (h) => h(App),
+}).$mount("#app");
+
