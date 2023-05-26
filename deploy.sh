b9665460db2bed85bb3abea3ff6dc9bcb9ba0ec3
tjk1898 <tjk1898@gmail.com>
2023-05-26 08:53:30 +0800

    修改部署

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,12 @@
-#!/bin/bash
+#!/bin/bash -e
 
 # 1. 本地npm打包vue程序
+if [ -d tmpdir ]; then rm -rf tmpdir; fi
+mkdir -p tmpdir
+cd tmpdir
 echo "正在进行本地npm打包vue程序..."
+git clone https://gitee.com/tjk1898/reggie-frontend-manager.git
+cd reggie-frontend-manager
 npm install
 npm run build
 cd dist
@@ -16,7 +21,7 @@ REMOTE_HOST="49.232.144.133"
 REMOTE_PATH="/root/frontend/manager"
 
 echo "正在连接远程服务器并清空目标目录..."
-ssh ${REMOTE_USER}@${REMOTE_HOST} "rm -rf ${REMOTE_PATH}/*"
+ssh ${REMOTE_USER}@${REMOTE_HOST} "if [ ! -d ${REMOTE_PATH} ]; then mkdir ${REMOTE_PATH}; else rm -rf ${REMOTE_PATH}/*; fi"
 echo "远程目录清空完成."
 
 # 3. 将build下的内容，发送到远端目录
@@ -28,5 +33,6 @@ ssh ${REMOTE_USER}@${REMOTE_HOST} "tar -zxvf ${REMOTE_PATH}/${LOCAL_BUILD_TAR_FI
 echo "内容已发送到远端目录."
 
 echo "清理本地文件"
-rm -rf dist.tar.gz
+cd ../../
+rm -rf tmpdir
 echo "脚本执行完毕."
